9bbdc88953dc674d68ede421096e92a499869ed5
unknown <you@example.com>
2024-12-21 12:20:55 +0100

    datatypes

---
+++ src/main/java/al/sda/datatypes/Main.java
@@ -0,0 +1,14 @@
+package al.sda;
+
+public class Main {
+    public static void main(String[] args) {
+        byte byteVariable = 175;
+        System.out.println("This is a byte variable: " + byteVariable);
+        short shortNumber = 12435;
+        System.out.println("This is a short number: " + shortNumber);
+        String text = "This is a string types";
+        System.out.println(text);
+    }
+}
+
+}
