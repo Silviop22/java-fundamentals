7939a905b7fd8ad27affe774416e4885031a7086
Elisabet <xhemollarielisabet@gmail.com>
2024-12-21 12:14:39 +0100

    Add datatypes package

---
+++ src/main/java/al/sda/datatypes/Main.java
@@ -0,0 +1,24 @@
+package al.sda.datatypes;
+
+public class Main {
+    public static void main(String[] args) {
+        byte byteVariable= 125;
+        System.out.println("This is a byte Variable: " + byteVariable);
+        short shortVariable= 126;
+        System.out.println("This is a short Variable: " + shortVariable);
+        int intVariable= 127;
+        System.out.println("This is a int Variable: " + intVariable);
+        long longVariable= 128;
+        System.out.println("This is a long Variable: " + longVariable);
+        float floatVariable= 127.5f;
+        System.out.println("This is a float Variable: " + floatVariable);
+        double doubleVariable= 127.5d;
+        System.out.println("This is a double Variable: " + doubleVariable);
+        char charVariable= 'a';
+        System.out.println("This is a char Variable: " + charVariable);
+        boolean booleanVariable= true;
+        System.out.println("This is a boolean Variable: " + booleanVariable);
+        String stringVariable= "Hello World";
+        System.out.println("This is a String Variable: " + stringVariable);
+    }
+}
