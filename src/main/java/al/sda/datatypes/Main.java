7ad702ec644e6453dffc2e3de3f3ea6865698aad
armirigj <armirgjini76@gmail.com>
2024-12-21 12:15:56 +0100

    @datatypes package

---
+++ src/main/java/al/sda/datatypes/Main.java
@@ -0,0 +1,13 @@
+package al.sda.datatypes;
+
+public class Main {
+    public static void main(String[] args){
+        byte byteVariable = 125;
+        System.out.println("This is atype variable " + byteVariable);
+        short shortVariable = 12344;
+        System.out.println("This is a short variable " + shortVariable);
+        String course = "Java49";
+        System.out.println("This is a string variable " + course);
+
+    }
+}
