469cbdc780bfab60d4546091ac1f09cea5e89311
Rei radheshi <reiradheshi@Eltons-MacBook-Pro.local>
2024-12-21 12:12:49 +0100

    Add datatypes package

---
+++ src/main/java/al/sda/datatypes/Main.java
@@ -0,0 +1,12 @@
+package al.sda.datatypes;
+
+public class Main {
+public static void main(String[] args) {
+    byte byteVariable = 125;
+    System.out.println("this is a byte variable" +byteVariable);
+    short shortNumber = 1234;
+    System.out.println("This is a short number" +shortNumber);
+    String text = "this is a string type";
+    System.out.println(text);
+}
+}
