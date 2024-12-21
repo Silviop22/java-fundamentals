cfc3ea5d349bcef1d662bc9d4f641f8ed3622357
Your Name <you@example.com>
2024-12-21 12:24:29 +0100

    Add datatypes package

---
+++ src/main/java/al/sda/datatype/Main.java
@@ -0,0 +1,13 @@
+package al.sda.datatype;
+
+public class Main {
+    public static void main(String[] args) {
+        byte byteVariable = 125;
+        System.out.println("This is a variable: " + byteVariable);
+        short shortNumber = 12345;
+        System.out.println("This is a short number: " + shortNumber);
+        String text = "This is a string type";
+        System.out.println(text);
+
+    }
+}
