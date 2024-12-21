9a47e2a7b65c7bdb0c4c80a88f8920085a8257b5
enisa-h <halilajenisa0@gmail.com>
2024-12-21 12:13:08 +0100

    Add datatypes package

---
+++ src/main/java/al/sda/datatypes/Main.java
@@ -0,0 +1,16 @@
+package al.sda.datatypes;
+
+public class Main {
+    public static void main(String[] args) {
+        byte byteVariable = 125;
+        System.out.println("This is a byte variable" + byteVariable);
+        short shortVariable = 283;
+        System.out.println("This is a short variable" + shortVariable);
+        int intVariable = 333;
+        System.out.println("This is a int variable" + intVariable);
+        String stringVariable = "Hillow Hillow";
+        System.out.println("This is a String variable" + stringVariable);
+
+
+    }
+}
